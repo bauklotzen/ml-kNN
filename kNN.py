abba141374414b8ffe0da2e80c80d8cd5ec62c88
asdfghjkl123 <504794537@qq.com>
2018-02-24 14:19:13 +0800

    fix some bug that some code rewrite in python3

--- kNN.py
+++ kNN.py
@@ -13,9 +13,7 @@ def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat**2
-    print(sqDiffMat)
     sqDistances = sqDiffMat.sum(axis=1)
-    print(sqDistances)
     distances = sqDistances**0.5
     
     # choose shortest k points
@@ -26,7 +24,7 @@ def classify0(inX, dataSet, labels, k):
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     
     # sort
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse = True)
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse = True)
 
     return sortedClassCount[0][0]
 
@@ -58,4 +56,16 @@ def autoNorm(dataSet):
     normDataSet = normDataSet/tile(ranges, (m, 1))
     return normDataSet, ranges, minVals
 
-
+def datingClassTest():
+    hoRatio = 0.1 #test set ratio 
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("The classifier came back with: %d, the real answer is: %d" %(classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("The total error rate is: %f"%(errorCount/float(numTestVecs)))
